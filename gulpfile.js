841bb8893ee2711578368cf969b80d97ddb424b5
Aleksandr <legionerab3000@yandex.ru>
2020-09-09 19:25:04 +0300

    Сделал каталог

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('images', function () {
 gulp.task('webp', function () {
   return gulp.src('source/img/**/*.{jpg,png}')
       .pipe(webp({quality: 90}))
-      .pipe(gulp.dest('source/img'));
+      .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('sprite', function () {
