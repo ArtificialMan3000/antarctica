6574a847c97a82a2a838e0dd094f4b5390da27b2
Aleksandr <legionerab3000@yandex.ru>
2020-09-19 00:00:53 +0300

    Внёс правки

--- gulpfile.js
+++ gulpfile.js
@@ -122,7 +122,8 @@ gulp.task('server', function () {
 
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
   gulp.watch('source/img/sprite-*.svg', gulp.series('sprite', 'html', 'refresh'));
-  gulp.watch('source/js/script.js', gulp.series('js', 'html', 'refresh'));
+  gulp.watch('source/js/**/*.js', gulp.series('js', 'html', 'refresh'));
+  gulp.watch('source/js/*.js', gulp.series('js', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
 });
 
